ac8d279b1c54b5f081fef644668e44cc93126301
tingxuan0911 <candy521927@yahoo.com.tw>
2024-06-20 14:49:26 +0800

    技巧:使用者反饋

--- src/components/skill.vue
+++ src/components/skill.vue
@@ -9,27 +9,27 @@ const device = inject("device");
      <div class="bg-[#fff] py-5 px-3">
           <div class="text-2xl font-bold mb-5">技能</div>
           <div class="flex flex-wrap gap-1 w-full">
-            <div class="w-[40%] rounded bg-[#E4E2E2] text-center px-2">
+            <div class="w-[40%] rounded bg-[#E4E2E2] text-center hover:shadow-inner hover:shadow-zinc-400 px-2">
               <div>前端基本</div>
               <div class="text-sm border border-t-[#735643]">HTML、CSS、SASS、JavaScript</div>
             </div>
-            <div class="w-[58%] rounded bg-[#E4E2E2] text-center px-2">
+            <div class="w-[58%] rounded bg-[#E4E2E2] text-center hover:shadow-inner hover:shadow-zinc-400 px-2">
               <div>VueJS</div>
               <div
                 class="text-sm border border-t-[#735643]"
               >Vue3、Vue2、Vue Router、Vue-Cil、Vuex、Pinia、Node.js、Ajax、Linux、API串接、Postman</div>
             </div>
-            <div class="w-[60%] rounded bg-[#E4E2E2] text-center px-2">
+            <div class="w-[60%] rounded bg-[#E4E2E2] text-center hover:shadow-inner hover:shadow-zinc-400 px-2">
               <div>套件</div>
               <div
                 class="text-sm border border-t-[#735643]"
               >Tailwind、Bootstrap、Element Plus、PrimeVue、i18n</div>
             </div>
-            <div class="w-[38%] rounded bg-[#E4E2E2] text-center px-2">
+            <div class="w-[38%] rounded bg-[#E4E2E2] text-center hover:shadow-inner hover:shadow-zinc-400 px-2">
               <div>版本控制</div>
               <div class="text-sm border border-t-[#735643]">Git、GitHub、GitLab</div>
             </div>
-            <div class="rounded bg-[#E4E2E2] text-center px-2 py-2">
+            <div class="rounded bg-[#E4E2E2] text-center hover:shadow-inner hover:shadow-zinc-400 px-2 py-2">
               <div class>RWD</div>
             </div>
           </div>
